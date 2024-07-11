0189d10e446982e777e2398852797f7ab151471e
shelendrarathore <writetoshelendra@gmail.com>
2024-07-11 23:04:10 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
 System.out.println("This is Java program");
 System.out.println("Output from java program");
 System.out.println(new Date());
+System.out.println("Hello world");
     }
 }}
